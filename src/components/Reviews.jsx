1e2b5c04886047f707ee81ac00000aea66b3cb95
Marivanenoronha <noronhamarivane4@gmail.com>
2024-10-03 18:41:19 -0300

    feat: change reviews.jsx and index.html

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,13 +1,29 @@
 
 import React from 'react';
+import { useEffect } from 'react';
 import { Card, CardBody, CardText, CardFooter, CardTitle } from "react-bootstrap";
 import "./Reviews.css";
 
 export function Reviews() {
-
+    useEffect(() => {
+        // Garantir que o script seja carregado corretamente
+        const script = document.getElementById('EmbedSocialScript');
+        if (!script) {
+            const newScript = document.createElement('script');
+            newScript.src = 'https://embedsocial.com/api/pro_hashtag/d6b2f10f90e923463380fc7bc3c3ecfc93c91067';
+            newScript.async = true;
+            newScript.id = 'EmbedSocialScript';
+            document.body.appendChild(newScript);
+        }
+    }, []);
 
     return (
         <div className="reviews-section container">
+
+            <div className="reviews-section container">
+                <h2 className="text-center mb-5 text-uppercase fw-bold fs-1">Avaliações</h2>
+                <div className="embedsocial-reviews" data-ref="d6b2f10f90e923463380fc7bc3c3ecfc93c91067"></div>
+            </div>
             <h2 className="text-center mb-5 text-upercase fw-bold fs-1">Avaliações</h2>
             <div className="row g-4">
                 <div className="col-lg-6">
